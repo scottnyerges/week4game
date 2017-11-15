82603f2b70584b251b2767d3f5599ec3e1719125
Scott Nyerges <Scott@Scotts-MacBook-Air.local>
2017-11-15 15:16:09 -0600

    finished files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,110 @@
+
+// THIS NEEDS TO HAPPEN FIRST
+
+// 	define starting point value of each crystal
+// 	define starting value of random number 
+
+// define starting value of total score
+var totalScore = 0;
+
+// define value of wins/losses; change values to 0
+var wins = 0;
+var losses = 0;
+
+// 	generate random values for crystals MATH between 1 and 12
+var crystal1 = Math.floor((Math.random() * 12) + 1); 
+var crystal2 = Math.floor((Math.random() * 12) + 1);
+var crystal3 = Math.floor((Math.random() * 12) + 1);
+var crystal4 = Math.floor((Math.random() * 12) + 1);
+
+// 	generate random number MATH between 19 and 120
+var randomNumber = Math.floor((Math.random() * 120) + 19);
+	// this sends the random number to the HTML to be displayed
+    document.getElementById("computerPick").innerHTML = randomNumber;
+    console.log(computerPick);
+
+// THIS HAPPENS NEXT; NEED TO GO INTO INNER FUNCTION AND RUN FIRST
+
+// 	player clicks crystal1
+document.getElementById("crystal1").onclick = function() {myFunction1()};
+// this adds crystal1's value to player's total score
+function myFunction1() {
+ 	// this displays the crystal's value; NEEDS TO ADD VALUE TO ITSELF
+ 	totalScore = (totalScore + crystal1);
+    document.getElementById("totalScore").innerHTML = totalScore;
+	console.log(crystal1);
+	// this function checks the score against the randomNumber
+	scoreCheck();
+}
+
+// 	player clicks crystal2
+document.getElementById("crystal2").onclick = function() {myFunction2()};
+// this adds crystal1's value to player's total score
+function myFunction2() {
+	// this displays the crystal's value
+	totalScore = (totalScore + crystal2);
+    document.getElementById("totalScore").innerHTML = totalScore;
+    console.log(crystal2);
+    // this function checks the score against the randomNumber
+    scoreCheck();
+}
+
+// 	player clicks crystal3
+document.getElementById("crystal3").onclick = function() {myFunction3()};
+// this adds crystal1's value to player's total score
+function myFunction3() {
+	// this displays the crystal's value
+	totalScore = (totalScore + crystal3);
+    document.getElementById("totalScore").innerHTML = totalScore;
+    console.log(crystal3);
+    // this function checks the score against the randomNumber
+    scoreCheck();
+}
+
+// 	player clicks crystal4
+document.getElementById("crystal4").onclick = function() {myFunction4()};
+// this adds crystal1's value to player's total score
+function myFunction4() {
+	// this displays the crystal's value
+	totalScore = (totalScore + crystal4);
+    document.getElementById("totalScore").innerHTML = totalScore;
+    console.log(crystal4);
+    // this function checks the score against the randomNumber
+    scoreCheck();
+}
+
+// this alerts the player to whether they win or lose and adds to the win/loss tally
+function scoreCheck() {
+	console.log("WE ARE HERE");
+
+	if (totalScore >= randomNumber) {
+	alert("You lose!");
+
+	    // Then we will loss and we'll update the HTML to display the loss.
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        // Then we'll call the reset.
+        reset();
+
+	}
+	else if (totalScore == randomNumber) {
+	alert("You win!");
+
+	    // If there is then we win and we'll update the HTML to display the win.
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        // Then we'll reset the game
+        reset();
+
+	}
+};	
+
+
+
+
+
+
+// game resets
+		// new random number generated 
+		// new crystal value generated
+		// total score is reset
